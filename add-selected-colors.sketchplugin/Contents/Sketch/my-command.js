03d0db3bf176b9b275bc04b16564f14f8f38fd65
Jorge Martins <hello@jorgemartins.me>
2019-03-06 20:18:53 +0000

    add description

--- add-selected-colors.sketchplugin/Contents/Sketch/my-command.js
+++ add-selected-colors.sketchplugin/Contents/Sketch/my-command.js
@@ -1,1 +1,188 @@
-var that=this;function __skpm_run(e,t){that.context=t;var r=function(e){var t={};function r(n){if(t[n])return t[n].exports;var o=t[n]={i:n,l:!1,exports:{}};return e[n].call(o.exports,o,o.exports,r),o.l=!0,o.exports}return r.m=e,r.c=t,r.d=function(e,t,n){r.o(e,t)||Object.defineProperty(e,t,{configurable:!1,enumerable:!0,get:n})},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="",r(r.s=0)}([function(e,r,n){"use strict";Object.defineProperty(r,"__esModule",{value:!0}),r.addJSON=function(){for(var e=0;e<u.length;e++){var t=c(u[e]);"#FFFFFF"===t&&(t=f(u[e]));var r=d(u[e].name()),n=p(u[e].name());h(t,r,n)}var o=NSSavePanel.savePanel();if(o.setNameFieldStringValue("palette.json"),o.setAllowedFileTypes(["json"]),o.setAllowsOtherFileTypes(!1),o.setExtensionHidden(!1),o.runModal()){var l=o.URL().path(),s=NSString.stringWithString(JSON.stringify([]));s.writeToFile_atomically_encoding_error(l,!0,NSUTF8StringEncoding,null)}},r.resetPalette=function(){a.setColorAssets([]),i.inspectorController().closeAnyColorPopover(),s.refreshCurrentDocument(),o.UI.message("Document Colors were Emptied!")};var o=n(1),l={},s=NSApp.delegate(),i=t.document,a=i.documentData().assets(),u=t.selection,c=function(e){return"#"+String(e.style().fills().firstObject().color().immutableModelObject().hexValue().toString())},f=function(e){var t=e.style().fills().firstObject().color().immutableModelObject(),r=Math.round(255*t.red()),n=Math.round(255*t.green()),o=Math.round(255*t.blue());return"rgba("+String(r)+","+String(n)+","+String(o)+","+String(t.alpha())+")"},d=function(e){return e.split("/")[0]},p=function(e){return e.split("/")[1].split("-")[1]},h=function(e,t,r){void 0===l[t]&&(l[t]={}),l[t][r]={value:e}};r.default=function(e){if(u.length>0){for(var t=[],r=0;r<u.length;r++){var n=MSColor.colorWithRed_green_blue_alpha(u[r].style().fills().firstObject().color().red(),u[r].style().fills().firstObject().color().green(),u[r].style().fills().firstObject().color().blue(),u[r].style().fills().firstObject().color().alpha()),l=d(u[r].name()),c=p(u[r].name()),f=MSColorAsset.alloc().initWithAsset_name(n,l+c);t.push(f)}a.addColorAssets(t),i.inspectorController().closeAnyColorPopover(),i.reloadInspector(),s.refreshCurrentDocument(),o.UI.message("Selected Colors added to Document Palette")}else o.UI.message("Select at least one Layer")}},function(e,t){e.exports=require("sketch")}]);"default"===e&&"function"==typeof r?r(t):r[e](t)}that.onRun=__skpm_run.bind(this,"default"),that.addJSON=__skpm_run.bind(this,"addJSON"),that.resetPalette=__skpm_run.bind(this,"resetPalette");
+var that = this;
+function __skpm_run (key, context) {
+  that.context = context;
+
+var exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.addJSON = addJSON;
+exports.resetPalette = resetPalette;
+var sketch = __webpack_require__(1);
+
+var colors = {};
+var app = NSApp.delegate();
+var doc = context.document;
+var documentData = doc.documentData();
+var assets = documentData.assets();
+var layers = context.selection;
+
+var getColorHex = function getColorHex(layer) {
+  return "#" + String(layer.style().fills().firstObject().color().immutableModelObject().hexValue().toString());
+};
+
+var getColorRgba = function getColorRgba(layer) {
+  var layerColor = layer.style().fills().firstObject().color().immutableModelObject();
+  var red = Math.round(layerColor.red() * 255);
+  var green = Math.round(layerColor.green() * 255);
+  var blue = Math.round(layerColor.blue() * 255);
+
+  return "rgba(" + String(red) + "," + String(green) + "," + String(blue) + "," + String(layerColor.alpha()) + ")";
+};
+
+var getColorName = function getColorName(colorName) {
+  return colorName.split("/")[0];
+};
+
+var getColorVariation = function getColorVariation(colorName) {
+  return colorName.split("/")[1].split("-")[1];
+};
+
+var addColor = function addColor(code, name, variation) {
+  if (colors[name] === undefined) colors[name] = {};
+  colors[name][variation] = { value: code };
+};
+
+exports["default"] = function (context) {
+  if (layers.length > 0) {
+    var _colors = [];
+    for (var i = 0; i < layers.length; i++) {
+
+      var colorCode = MSColor.colorWithRed_green_blue_alpha(layers[i].style().fills().firstObject().color().red(), layers[i].style().fills().firstObject().color().green(), layers[i].style().fills().firstObject().color().blue(), layers[i].style().fills().firstObject().color().alpha());
+      var colorName = getColorName(layers[i].name());
+      var colorVariation = getColorVariation(layers[i].name());
+      var newColor = MSColorAsset.alloc().initWithAsset_name(colorCode, colorName + colorVariation);
+      _colors.push(newColor);
+    }
+    assets.addColorAssets(_colors);
+    doc.inspectorController().closeAnyColorPopover();
+    doc.reloadInspector();
+    app.refreshCurrentDocument();
+
+    sketch.UI.message("Selected Colors added to Document Palette");
+  } else {
+    sketch.UI.message("Select at least one Layer");
+  }
+};
+
+function addJSON() {
+  var colors = [];
+  for (var i = 0; i < layers.length; i++) {
+    var colorCode = getColorHex(layers[i]);
+    if (colorCode === "#FFFFFF") {
+      colorCode = getColorRgba(layers[i]);
+    };
+    var colorName = getColorName(layers[i].name());
+    var colorVariation = getColorVariation(layers[i].name());
+    addColor(colorCode, colorName, colorVariation);
+  }
+  // log JSON file
+  var save = NSSavePanel.savePanel();
+  save.setNameFieldStringValue("palette.json");
+  save.setAllowedFileTypes(["json"]);
+  save.setAllowsOtherFileTypes(false);
+  save.setExtensionHidden(false);
+  if (save.runModal()) {
+    var filePath = save.URL().path();
+    var file = NSString.stringWithString(JSON.stringify(colors));
+
+    file.writeToFile_atomically_encoding_error(filePath, true, NSUTF8StringEncoding, null);
+  };
+};
+
+function resetPalette() {
+  assets.setColorAssets([]);
+  doc.inspectorController().closeAnyColorPopover();
+  app.refreshCurrentDocument();
+  sketch.UI.message("Document Colors were Emptied!");
+}
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+module.exports = require("sketch");
+
+/***/ })
+/******/ ]);
+  if (key === 'default' && typeof exports === 'function') {
+    exports(context);
+  } else {
+    exports[key](context);
+  }
+}
+that['onRun'] = __skpm_run.bind(this, 'default');
+that['addJSON'] = __skpm_run.bind(this, 'addJSON');
+that['resetPalette'] = __skpm_run.bind(this, 'resetPalette')
